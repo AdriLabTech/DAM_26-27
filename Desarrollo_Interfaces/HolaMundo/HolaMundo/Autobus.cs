2f353eb9fe89612b808b23b2572f0be3a692bcab
AdriLabTech <labs.lownoise@gmail.com>
2026-09-22 16:50:44 +0200

    Feature(Desarrollo_Interfaces): Agregamos el modulo de Desarrollo de Intefaces, con los primeros proyectos de prueba e iniciacion al lenguaje (C#)

---
+++ Desarrollo_Interfaces/HolaMundo/HolaMundo/Autobus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HolaMundo
+{
+    internal class Autobus : Transporte
+    {
+
+        public Autobus(string capacidad, string ruta) : base(capacidad, ruta) { }
+    }
+}
---
+++ Desarrollo_Interfaces/Pruebas/Pruebas/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Ejercicio_Hola_C();
+
+        Ejercicio_Datos_Personales();
+
+
+    }
+
+    static void Ejercicio_Datos_Personales()
+    {
+        Console.WriteLine("Como te llamas?: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("Cual es tu edad?: ");
+        int edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Cuanto mides?: ");
+        double altura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Eres estudiante? (s/n): ");
+        bool esEstudiante = Console.ReadLine().ToLower() == "s";
+
+        string statusEstudante = esEstudiante ? "soy estudiante" : "no soy estudiante";
+
+        Console.WriteLine($"Me llamo {nombre}, tengo {edad} años, mido {altura}m  y {statusEstudante}");
+    }
+
+    static void Ejercicio_Hola_C()
+    {
+        string nombreMostrar = "Carlos";
+
+        Console.WriteLine($"Hola, soy {nombreMostrar}");
+        Console.WriteLine("Estoy aprendiendo C#");
+    }
+}
